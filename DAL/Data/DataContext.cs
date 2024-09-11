7454bc09e47ae3bd7d74454b45762f69be97f608
Md.Ariful Islam <butsit20@gmail.com>
2024-09-11 16:47:28 +0600

    add course table

--- DAL/Data/DataContext.cs
+++ DAL/Data/DataContext.cs
@@ -10,6 +10,7 @@ namespace DAL.Data
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
